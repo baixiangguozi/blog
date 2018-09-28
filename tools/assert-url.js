ac420ef2f735deac9459c6848c5271ab1c2899b3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-28 15:05:24 +0000

    Deploy baixiangguozi/blog to github.com/baixiangguozi/blog.git:gh-pages

--- tools/assert-url.js
+++ tools/assert-url.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 
 var imgRg = /!\[([^\]]*)\]\((\/[^/]\S*)\)/gm;
 var basePath = 'https://raw.githubusercontent.com/kitian616/jekyll-TeXt-theme/master';
-var srcPaths = ['./test/_posts', './docs/en', './docs/zh'];
+var srcPaths = ['./test/_posts', './docs/en', './docs/zh', './notes'];
 
 function runner() {
   srcPaths.forEach(function(srcPath) {
